a1fb84cc682bb6403027c5e5070d53d7902e3598
adv-panda <98755510+adv-panda@users.noreply.github.com>
2024-07-24 08:16:12 +0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'XAI-DESReg'
+copyright = '2024, Panda'
+author = 'Panda'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.1.1'
 
 # -- General configuration
 
